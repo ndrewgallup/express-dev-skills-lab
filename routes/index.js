9f8f9ec67607f3b846c5dc135057e579c87aecdf
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-02-27 12:23:27 -0600

    add models and controllers

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 const router = Router()
 
 /* GET home page. */
+//localhost:3000
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' })
 })
